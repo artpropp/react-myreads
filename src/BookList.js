5d0b9b23fbcc9bf361319b23e18b56a928230550
Artur Propp <artur.propp@me.com>
2018-04-09 21:02:21 +0200

    feat: Add link to open seach
    The appearance of the router link is a  button as in the starter template.
    The button appears in the bottom right corner of the page.

--- src/BookList.js
+++ src/BookList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Bookshelf from './Bookshelf';
 
@@ -16,29 +17,36 @@ class BookList extends Component {
     const { books, onBookChanged } = this.props;
 
     return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
+      <div>
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <Bookshelf
+              key="currentlyReading"
+              title="Currently Reading"
+              books={books.filter(book => book.shelf === 'currentlyReading')}
+              onBookChanged={onBookChanged}
+            />
+            <Bookshelf
+              key="wantToRead"
+              title="Want to Read"
+              books={books.filter(book => book.shelf === 'wantToRead')}
+              onBookChanged={onBookChanged}
+            />
+            <Bookshelf
+              key="read"
+              title="Read"
+              books={books.filter(book => book.shelf === 'read')}
+              onBookChanged={onBookChanged}
+            />
+          </div>
         </div>
-        <div className="list-books-content">
-          <Bookshelf
-            key="currentlyReading"
-            title="Currently Reading"
-            books={books.filter(book => book.shelf === 'currentlyReading')}
-            onBookChanged={onBookChanged}
-          />
-          <Bookshelf
-            key="wantToRead"
-            title="Want to Read"
-            books={books.filter(book => book.shelf === 'wantToRead')}
-            onBookChanged={onBookChanged}
-          />
-          <Bookshelf
-            key="read"
-            title="Read"
-            books={books.filter(book => book.shelf === 'read')}
-            onBookChanged={onBookChanged}
-          />
+        <div className="open-search">
+          <Link
+            to="/search"
+          >Add a book</Link>
         </div>
       </div>
     )
